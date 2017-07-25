fa1384dd4af56882e31ac17c4d7e760479b5bb03
lls <1942625600@qq.com>
2017-07-25 14:17:45 +0800

    service add

---
+++ spring-cloud-microservice-study/microservice-discovery-eureka/src/main/java/com/springcloud/lls/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.springcloud.lls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka发现服务
+ * 
+ * @author LLS
+ * 
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+}
